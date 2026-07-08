bdac101939ee243461531c824f7661a323736f0b
sparkox1-chinu <sparkox19711@gmail.com>
2026-07-08 22:00:42 +0530

    Update next.config.js

--- ai-assistant/next.config.js
+++ ai-assistant/next.config.js
@@ -1,1 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.pollinations.ai",
+      },
+    ],
+  },
+};
 
+module.exports = nextConfig;
